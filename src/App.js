0a459bec1a563647a66d39833a706db258afa12b
maryamkaramad <maryamkaramad1373@gmail.com>
2023-01-10 10:25:01 +0330

    react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import { useState } from 'react';
-import './App.css';
-import { Contacts, Navbar } from "./components"
 
+import './App.css'
+import { useState } from 'react';
+import { Contacts, Navbar, ViewContact } from "./components"
+import { Routes, Route, Navigate } from 'react-router-dom';
+import AddContact from './components/Contacts/AddContact';
+import EditContact from './components/Contacts/EditContact';
 
 const App = () => {
   const [getContacts, setGetContacts] = useState([])
@@ -9,7 +12,14 @@ const App = () => {
   return (
     <div className='App'>
       <Navbar />
-      <Contacts contacts={getContacts} loading={loading} />
+      <Routes>
+        <Route path='/' element={<Navigate to="/contacts" />} />
+        <Route path='/contacts' element={<Contacts contacts={getContacts} loading={loading} />} />
+        <Route path='/contacts/add' element={<AddContact />} />
+        <Route path='/contacts/:contactId' element={<ViewContact />} />
+        <Route path='/contacts/edit:contactId' element={<EditContact />} />
+      </Routes>
+
     </div>
   );
 }
