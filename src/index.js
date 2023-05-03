75b780d27d07b7c1c16d6a38cf0e1a62966245ea
maryamkaramad <maryamkaramad1373@gmail.com>
2023-05-03 22:59:38 +0330

    react-error-boundary

--- src/index.js
+++ src/index.js
@@ -6,16 +6,37 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import 'react-confirm-alert/src/react-confirm-alert.css'; // Import css
 import 'react-toastify/dist/ReactToastify.css';
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+import { ErrorBoundary } from "react-error-boundary";
 
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+
+function Fallback({ error, resetErrorBoundary }) {
+  // Call resetErrorBoundary() to reset the error boundary and retry the render.
+
+  return (
+    <div role="alert">
+      <p>Something went wrong:</p>
+      <pre style={{ color: "red" }}>{error.message}</pre>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <ErrorBoundary
+    FallbackComponent={Fallback}
+    onReset={(details) => {
+      // Reset the state of your app so the error doesn't happen again
+    }}
+  >
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ErrorBoundary>
 
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
